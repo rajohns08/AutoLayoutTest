09863d88fa10d3049984368cf35d367c723348ea
rajohns <rajohns@bcbsms.com>
2014-10-27 16:12:29 -0500

    Initial Commit

---
+++ AutoLayoutTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutTest
+//
+//  Created by Adam Johns on 10/27/14.
+//  Copyright (c) 2014 Adam Johns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AutoLayoutTest/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  AutoLayoutTest
+//
+//  Created by Adam Johns on 10/27/14.
+//  Copyright (c) 2014 Adam Johns. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
